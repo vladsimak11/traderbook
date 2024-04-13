65240af613889a41ca30351a3113d6857c31825e
Vladyslav Simak <simakv21@gmail.com>
2024-04-13 21:38:54 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store= {store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="/traderbook">
           <App />
         </BrowserRouter>
       </PersistGate>
